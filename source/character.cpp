b2911c0e9405739a562836c70223c3e430b18a49
Blaine Konyn McDonald <133623777+McDeevious@users.noreply.github.com>
2025-06-17 01:53:37 +1200

    Character abstraction

---
+++ source/character.cpp
@@ -0,0 +1,13 @@
+#include "character.h"
+
+// Empty abstract class, just want it for its behaviour
+
+Character::Character()
+{
+
+}
+
+Character::~Character()
+{
+
+}
---
+++ source/character.h
@@ -0,0 +1,42 @@
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+#include "vector2.h"
+#include "collision.h"
+#include <fmod.hpp>
+#include "sharedenums.h"
+
+class Renderer;
+class InputSystem;
+class Hitbox;
+
+class Character {
+public:
+    Character();
+    virtual ~Character() {}
+
+    // Core interface
+    virtual bool Initialise(Renderer& renderer) = 0;
+    virtual void Process(float deltaTime) = 0;
+    virtual void Draw(Renderer& renderer) = 0;
+    virtual void ProcessInput(InputSystem& inputSystem) = 0;
+
+    // State and logic
+    virtual void SetBoundaries(float left, float right, float top, float bottom) = 0;
+    virtual const Vector2& GetLastMovementDirection() const = 0;
+    virtual const Vector2& GetPosition() const = 0;
+    virtual int GetHealth() const = 0;
+    virtual void TakeDamage(int amount) = 0;
+    virtual int AttackDamage() const = 0;
+    virtual bool isAttacking() const = 0;
+    virtual bool IsDead() const = 0;
+
+    // Optional
+    virtual bool isBlocking() const { return false; }
+
+    // Collision
+    virtual Hitbox GetHitbox() const = 0;
+    virtual Hitbox GetAttackHitbox() const = 0;
+};
+
+#endif // CHARACTER_H
